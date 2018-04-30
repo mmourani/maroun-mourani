2583161a791477f36cde3c598bcd05629f4d66b5
Maroun Mourani <mouranim@gmail.com>
2018-04-30 23:49:22 +0400

    added more content
    added more content on testimonials and other details

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -198,114 +198,155 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 <!-- Skills -->
 <section class="skills" id="skills">
     <div class="container">
-            <h3 class="heading">Skills</h3>
+            <h3 class="heading">Expertise</h3>
         <div class='col-md-8 bar'>
-        <p class="bar-p">Phasellus dignissim elit at ligula laoreet iaculis. Sed urna purus, 
-            convallis sed rhoncus eget, bibendum idtortor. Donec libero tellus, 
-            feugiat ut felis ac, commodo pharetra massa.</p>
+        <p class="bar-p">Dynamic and dedicated business development and marketing professional; with more than twenty years of international experience. Demonstrated success in developing and implementing complex technical solutions in highly competitive markets; known for the ability to routinely exceed expectations.</p>
             <div class="col-md-6 group">
                 <div class='bar_left'>
                     <div class='bar_group'>
-                        <div class='bar_group__bar thin' label='Business Dev' show_values='true' value='343'></div>
-                        <div class='bar_group__bar thin' label='Defense' show_values='true' value='235'></div>
-                        <div class='bar_group__bar thin' label='Software dev' show_values='true' value='245'></div>
+                        <div class='bar_group__bar thin' label='international BD and Sales' show_values='false' value='600'></div>
+                        <div class='bar_group__bar thin' label='Blockchain' show_values='false' value='600'></div>
+                        <div class='bar_group__bar thin' label='Software dev' show_values='false' value='600'></div>
+                        <div class='bar_group__bar thin' label='System solutions' show_values='false' value='600'></div>
+                        <div class='bar_group__bar thin' label='Growth Management' show_values='false' value='600'></div>
+                        <div class='bar_group__bar thin' label='Budgeting and forecasting' show_values='false' value='600'></div>
                     </div>
                 </div>
             </div>
             <div class="col-md-6 group">
                 <div class='bar_right'>
                     <div class='bar_group'>
-                        <div class='bar_group__bar thin' label='blockchain' show_values='true' value='675'></div>
-                        <div class='bar_group__bar thin' label='IOT' show_values='true' value='343'></div>
-                        <div class='bar_group__bar thin' label='AI' show_values='true' value='245'></div>
+                        <div class='bar_group__bar thin' label='Defense' show_values='false' value='600'></div>
+                        <div class='bar_group__bar thin' label='IOT' show_values='false' value='600'></div>
+                        <div class='bar_group__bar thin' label='AI' show_values='false' value='600'></div>
+                        <div class='bar_group__bar thin' label='ICO' show_values='false' value='600'></div>
+                        <div class='bar_group__bar thin' label='Staff training & Development' show_values='false' value='600'></div>
+                        <div class='bar_group__bar thin' label='Trilingual: English, French, Arabic' show_values='false' value='600'></div>
                     </div>
                 </div>
             </div>
             <div class='clearfix'></div>
         </div>
         <div class="col-md-4 skillsright">
-            <h3 class="bar-h3">About my skills</h3>
-            <h5 class="bar-p"><i>Phasellus dignissim elit at ligula laoreet iaculis. Sed urna purus</i></h5>
-            <p class="bar-p">Phasellus dignissim elit at ligula laoreet iaculis. Sed urna purus, 
-                convallis sed rhoncus eget, bibendum idtortor. Donec libero tellus, 
-                feugiat ut felis ac, commodo pharetra massa.</p>
-            <p class="bar-p">Bibendum idtortor. Donec libero tellus, 
-                feugiat ut felis ac, commodo pharetra massa.</p>
+            <h3 class="bar-h3">About my Expertise</h3>
+            <h5 class="bar-p"><i>Proven track record of successfully translating customer needs across several cultures in order to achieve project objectives, able to facilitate communications between stakeholders at all levels. </i></h5>
+            <p class="bar-p">Possesses outstanding leadership skills, adept providing the support, direction, and training needed to launch new business endeavors and achieve key operational goals. Effectively manages competing priorities in results driven environments.</p>
         </div>
         <div class='clearfix'></div>
     </div>
 </section>
-<!-- //Skills -->
+<!-- //Expertise -->
 
-<!-- gallery -->
-    <div id="gallery" class="gallery">
-        <h3 class="heading">Portfolio</h3>
-            <div class="gallery-info">
-                <div class="col-md-3 gallery-grids">
-                    <a href="images/gallery1.jpg" class="gallery-box" data-lightbox="example-set" data-title="">
-                        <img src="images/gallery1.jpg" alt="" class="img-responsive zoom-img">
-                    </a>
-                </div>
-                <div class="col-md-3 gallery-grids">
-                    <a href="images/gallery2.jpg" class="gallery-box" data-lightbox="example-set" data-title="">
-                        <img src="images/gallery2.jpg" alt="" class="img-responsive zoom-img">
-                    </a>
-                </div>
-                <div class="col-md-3 gallery-grids">
-                    <a href="images/gallery3.jpg" class="gallery-box" data-lightbox="example-set" data-title="">
-                        <img src="images/gallery3.jpg" alt="" class="img-responsive zoom-img">
-                    </a>
-                </div>
-                <div class="col-md-3 gallery-grids">
-                    <a href="images/gallery4.jpg" class="gallery-box" data-lightbox="example-set" data-title="">
-                        <img src="images/gallery4.jpg" alt="" class="img-responsive zoom-img">
-                    </a>
-                </div>
-                <div class="col-md-3 gallery-grids grid-mdl">
-                    <a href="images/gallery5.jpg" class="gallery-box" data-lightbox="example-set" data-title="">
-                        <img src="images/gallery5.jpg" alt="" class="img-responsive zoom-img">
-                    </a>
-                </div>
-                <div class="col-md-3 gallery-grids grid-mdl">
-                    <a href="images/gallery6.jpg" class="gallery-box" data-lightbox="example-set" data-title="">
-                        <img src="images/gallery6.jpg" alt="" class="img-responsive zoom-img">
-                    </a>
-                </div>
-                <div class="col-md-3 gallery-grids grid-mdl">
-                    <a href="images/gallery7.jpg" class="gallery-box" data-lightbox="example-set" data-title="">
-                        <img src="images/gallery7.jpg" alt="" class="img-responsive zoom-img">
-                    </a>
-                </div>
-                <div class="col-md-3 gallery-grids grid-mdl">
-                    <a href="images/gallery8.jpg" class="gallery-box" data-lightbox="example-set" data-title="">
-                        <img src="images/gallery8.jpg" alt="" class="img-responsive zoom-img">
-                    </a>
-                </div>
-                <div class="col-md-3 gallery-grids grid-mdl">
-                    <a href="images/gallery9.jpg" class="gallery-box" data-lightbox="example-set" data-title="">
-                        <img src="images/gallery9.jpg" alt="" class="img-responsive zoom-img">
-                    </a>
-                </div>
-                <div class="col-md-3 gallery-grids grid-mdl">
-                    <a href="images/gallery10.jpg" class="gallery-box" data-lightbox="example-set" data-title="">
-                        <img src="images/gallery10.jpg" alt="" class="img-responsive zoom-img">
-                    </a>
-                </div>
-                <div class="col-md-3 gallery-grids grid-mdl">
-                    <a href="images/gallery11.jpg" class="gallery-box" data-lightbox="example-set" data-title="">
-                        <img src="images/gallery11.jpg" alt="" class="img-responsive zoom-img">
-                    </a>
-                </div>
-                <div class="col-md-3 gallery-grids grid-mdl">
-                    <a href="images/gallery5.jpg" class="gallery-box" data-lightbox="example-set" data-title="">
-                        <img src="images/gallery5.jpg" alt="" class="img-responsive zoom-img">
-                    </a>
-                </div>
-                <div class="clearfix"> </div>   
+<div class="testimonials" id="testimonials">
+    <div class="container">
+            <h3 class="heading">Testimonials</h3>
+        <div class="col-md-6">
+            <div class="author">
+                <img src="images/references/Omar-El-Harrache.png" alt="Avatar" class="avatar img-circle pull-left">
+                <p class="text-right">
+                    Omar El Harrash
+                    <br>
+                    <small class="text-right">CEO Global Innovation Technologies Inc</small>
+                </p>
+                <p>
+                    <label>"المهندس مارون شخص جدير باالثقة ويدير ملفاته بعناية ويمتلك قدرة فائقة على التواصل مع الآخرين بسهولة. إنه يملك شبكة علاقات تجارية واسعة في العديد من الدول وبالاخص دول الخليج العربي.إذا أردت تنمية أعمالك وتوسيع تجارتك عبر أناس ذوي ثقة وخبرة وذكاء فأنصحك بالإتصال فورا بصديقنا المهندس مارون."</label>
+                </p>
             </div>
-        <script src="js/lightbox-plus-jquery.min.js"></script>
-    </div>
-<!-- //gallery -->
+        </div>
+        <div class="col-md-6">
+            <div class="author">
+                <img src="images/references/Bruce Lawson.png" alt="Avatar" class="avatar img-circle pull-left">
+                <p class="text-right">
+                    Bruce Lawson
+                    <br>
+                    <small class="text-right">Telecom and IT Executive - NetConnect</small>
+                </p>
+                <p>
+                    <label>"Maroun was and is a very resourceful and decidated professional. He focused on the customer and the objectives that were established. He was always able to accomplish these objectives on time and budget. Maroun is the consumate professional, dependable and willing to do the hard work necessary. His customer skills are superb and he would be a great addition to any organization."</label>
+                </p>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="author">
+                <img src="images/references/Bruno Monfils.png" alt="Avatar" class="avatar img-circle pull-left">
+                <p class="text-right">
+                    Bruno Monfils
+                    <br>
+                    <small class="text-right">Senior Advisor - AT Kearney</small>
+                </p>
+                <p>
+                    <label>"A talented software/hardware engineer with outstanding business development acumen, Maroun is an innovative thinker. To meet his business develpment objectives, he proposed the business case and successfully introduced the first VoIP gateway at Call-Net/Sprint Canada. As champion for this initiative thanks to his technical know how, Maroun was key in defusing engineering concerns."</label>
+                </p>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="author">
+                <img src="images/references/Ghassan-Abou-Rjeily.png" alt="Avatar" class="avatar img-circle pull-left">
+                <p class="text-right">
+                    Ghassan Abou Rjeili
+                    <br>
+                    <small class="text-right">VP Sales, Marketing - Business Development at ITQAN</small>
+                </p>
+                <p>
+                    <label>"Maroun is what a company needs to progress in a "challenging environment" such as the UAE. I've had the opportunity and privilege to work with Maroun on several projects. What really amazed me is the dedication shown on the ground. Maroun was always there ensuring the proper deliverables with all objectives properly passed to the customer with high level of satisfaction. Strategically, Maroun had very strong knowledge of his competition and potential opportunities in the region and had contacts with everyone. Working with him increased our strength in the region and improved our reputation of strong system integrators."</label>
+                </p>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="author">
+                <img src="images/references/Hedia Bahri.png" alt="Avatar" class="avatar img-circle pull-left">
+                <p class="text-right">
+                    Hedia Bahri
+                    <br>
+                    <small class="text-right">Targipsum Technologies Inc. - ROADNET PLANNING - ROUTING-DISPATCHING-TELEMATIC</small>
+                </p>
+                <p>
+                    <label>"I had the pleasure to work with Maroun @ Call-Net and he was my first contact at the time. Maroun is a VERY smart individual that has a strong and impressive technical background. Not only great in business but with people and he never loose a relationship. I recommand Maroun to anyone in the planet that needs the job done with honesty and dedication. I will work with Maroun again if I have the opportunity to do so!!!"</label>
+                </p>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="author">
+                <img src="images/references/Hisham-Baker.png" alt="Avatar" class="avatar img-circle pull-left">
+                <p class="text-right">
+                    Hisham Baker
+                    <br>
+                    <small class="text-right">Business Development Director at CDC Communications Development Company CDC</small>
+                </p>
+                <p>
+                    <label>"I have had the pleasure of getting to know Maroun over the past 8 years in my Business Development role at CDC".Maroun is a dynamic leader with an excellent sense of what a company needs to be successful. Besides being a sales person’s best friend, Maroun’s dedication and customer support is the best I have ever experienced. Many times I have taken advantage of Maroun’s world class knowledge of the market and communications capabilities to be able to propose the best solutions to our customers. I have found Maroun to be honest and will tell me if his products will do what the customer wants and if not, he suggests alternatives I can use."</label>
+                </p>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="author">
+                <img src="images/references/Jacques-Mc-Neill.png" alt="Avatar" class="avatar img-circle pull-left">
+                <p class="text-right">
+                    Jacques Mc Neill
+                    <br>
+                    <small class="text-right">Experienced high tech start-up entrepreneur</small>
+                </p>
+                <p>
+                    <label>"Mr. Mourani proved to be a very autonomous employee at a time where the Company's was initiating its activities and when resources were scarse. Presented with a global mandate, he diligently elaborated the process of actions, identified the customer's needs and executed the necessary tasks to secure new customers and provide them with the solutions for their unique requirements. I recommend his project management skills as well as his facility to initiate close human relationships with his customers."</label>
+                </p>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="author">
+                <img src="images/references/Jim-Dismukes.png" alt="Avatar" class="avatar img-circle pull-left">
+                <p class="text-right">
+                    Jim Dismukes
+                    <br>
+                    <small class="text-right">Program Manager at Harris Corporation</small>
+                </p>
+                <p>
+                    <label>"Mr. Mourani is a very personable individual and is well connected throughout the Middle East. He was very helpful and supportive in helping my group identify new opportunities in UAE and setting up meetings with potential customers. If you are in need of business development and marketing services in the Middle East, I highly recommend you contact Mr. Mourani"</label>
+                </p>
+            </div>
+        </div>
+</div>
+<!-- //Testimonials -->
+
 
 <!-- Education -->
 <div class="education" id="education">
