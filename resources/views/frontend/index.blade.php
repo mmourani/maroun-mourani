c873aeffe9ef308bb008d3608325c2fb0133fd2e
Maroun Mourani <mouranim@gmail.com>
2018-05-16 11:23:15 +0400

    updated education section

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -352,43 +352,34 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 <div class="education" id="education">
     <div class="container">
             <h3 class="heading">Education</h3>
-        <div class="col-md-6">
-            <div class="grid1">
-                <h3>Bachelor Degree of education</h3>
-                <p>Dignissim elit at ligula laoreet iaculis. Sed urna purus, 
-                    convallis sedrhon cus eget, bibendum idtortor. Donec libero tellus, 
-                    feugiat ut felis ac, commodo pharetra massa</p>
-                <h4><b>Year</b> : 2011-2013</h4>
+            <div class="col-md-6">
+                <div class="grid1">
+                    <h3>Blockchain, Software development and frameworks</h3>
+                    <p>continuous learning and practice of different programming languages and framework. Blockchain development, hyperledger, sawtooth, R3 corda, Openchain, Multichain, quorum, IOTA, Ethereum, CR-20, C, C++, Object Oriented Database, HTML, CSS, PHP, Laravel, front end development, back end development, GO, solidity, kotlin, Java, Open Zeppelin, truffle, Web3.js, Embark, populous raiden, oxchange, bancor and more</p>
+                    <h4><b>Year</b> : continuous Learning</h4>
+                </div>
             </div>
-        </div>
         <div class="col-md-6">
             <div class="grid1">
-                <h3>Bachelor Degree of education</h3>
-                <p>Dignissim elit at ligula laoreet iaculis. Sed urna purus, 
-                    convallis sedrhon cus eget, bibendum idtortor. Donec libero tellus, 
-                    feugiat ut felis ac, commodo pharetra massa</p>
-                <h4><b>Year</b> : 2011-2013</h4>
+                <h3>TAS Leadership program</h3>
+                <p>Intensive leadership development experience that improved my ability to help organization discover opportunities and sustain success in a complex region. Structured to maximize learning and minimize time away from work, enables organizations to prepare leaders for the challenges ahead while maintaining business momentum. At the same time, the program connects you with an exceptional network of experienced, driven peers from across the Middle East and beyond.</p>
+                <h4><b>Year</b> : 2010</h4>
             </div>
         </div>
         <div class='clearfix'></div>
         <div class="educationbottom">
-            <div class="col-md-6">
-                <div class="grid1">
-                    <h3>Bachelor Degree of education</h3>
-                    <p>Dignissim elit at ligula laoreet iaculis. Sed urna purus, 
-                        convallis sedrhon cus eget, bibendum idtortor. Donec libero tellus, 
-                        feugiat ut felis ac, commodo pharetra massa</p>
-                    <h4><b>Year</b> : 2011-2013</h4>
-                    
-                </div>
+        <div class="col-md-6">
+            <div class="grid1">
+                <h3>Cyber Security Master Courses</h3>
+                <p>Develop expertise to make informed and strategic decisions in the context of cybersecurity of computer networks.Apply best practices in the defense, protection and maintenance of the integrity of commercial and institutional computer networks on the Internet.</p>
+                <h4><b>Year</b> : 2013</h4>
             </div>
+        </div>
             <div class="col-md-6">
                 <div class="grid1 lastgrid">
-                    <h3>Bachelor Degree of education</h3>
-                    <p>Dignissim elit at ligula laoreet iaculis. Sed urna purus, 
-                        convallis sedrhon cus eget, bibendum idtortor. Donec libero tellus, 
-                        feugiat ut felis ac, commodo pharetra massa</p>
-                    <h4><b>Year</b> : 2011-2013</h4>
+                    <h3>Bachelor Degree in Computer engineering</h3>
+                    <p>Computer engineering is a discipline that integrates several fields of computer science and electronics engineering required to develop computer hardware and software.Training in electronic engineering (or electrical engineering), software design, and hardware–software integration.This field of engineering not only focuses on how computer systems themselves work, but also how they integrate into the larger picture.</p>
+                    <h4><b>Year</b> : 1992-1996</h4>
                 </div>
             </div>
             <div class='clearfix'></div>
