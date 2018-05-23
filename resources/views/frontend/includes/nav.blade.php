29c84480f39c16b68138c0e5bff8171d56881664
Maroun Mourani <mouranim@gmail.com>
2018-05-23 11:04:28 +0400

    debuging topbar now

--- resources/views/frontend/includes/nav.blade.php
+++ resources/views/frontend/includes/nav.blade.php
@@ -1,71 +1,57 @@
 
 <!-- Header -->
+    <div class="aitsheaderw3ls" id="agilehome">
         <!-- Navigation -->
-    <div class="agiletopbar">
-        <div class="wthreenavigation">
+        <div class="agiletopbar">
 
-                        <div class="menu-wrap">
+            <div class="wthreenavigation">
+                <div class="menu-wrap">
+                    <nav class="menu">
+                        <div class="icon-list">
 
-                            <nav class="menu">
+                            @if (Active::checkroute('frontend.index'))
+                            <a class="scroll active" href="#agilehome"><i class="fa fa-home"></i><span>Home</span></a>
+                            <a class="scroll" href="#about"><i class="fa fa-info"></i><span>About</span></a>
+                            <a class="scroll" href="#skills"><i class="fa fa-bar-chart"></i><span>Skills</span></a>
+                            <a class="scroll" href="#testimonials"><i class="fa fa-picture-o"></i><span>Testimonials</span></a>
+                            <a class="scroll" href="#education"><i class="fa fa-book"></i><span>Education</span></a>
+                            <a class="scroll" href="#experience"><i class="fa fa-university"></i><span>Experience</span></a>
+                            <a class="scroll" href="#contact"><i class="fa fa-phone"></i><span>Contact Me</span></a>
+                            <br>
+                            @else
+                            <a href="/"><i class="fa fa-home"></i><span>Home</span></a>
+                            <br>
+                            @endif
 
-                                <div class="icon-list">
+                            @guest
+                                <a href="{{route('frontend.auth.login')}}" class="nav-link {{ active_class(Active::checkRoute('frontend.auth.login')) }}"><i class="fa fa-sign-in"></i><span>{{ __('navs.frontend.login') }}</span></a>
 
-                                    @if (Active::checkroute('frontend.index'))
-                                    <a class="scroll active" href="#agilehome"><i class="fa fa-home"></i><span>Home</span></a>
-                                    <a class="scroll" href="#about"><i class="fa fa-info"></i><span>About</span></a>
-                                    <a class="scroll" href="#skills"><i class="fa fa-bar-chart"></i><span>Skills</span></a>
-                                    <a class="scroll" href="#testimonials"><i class="fa fa-picture-o"></i><span>Testimonials</span></a>
-                                    <a class="scroll" href="#education"><i class="fa fa-book"></i><span>Education</span></a>
-                                    <a class="scroll" href="#experience"><i class="fa fa-university"></i><span>Experience</span></a>
-                                    <a class="scroll" href="#contact"><i class="fa fa-phone"></i><span>Contact Me</span></a>
-                                    <br>
-                                    @else
-                                    <a href="/"><i class="fa fa-home"></i><span>Home</span></a>
-                                    <br>
-                                    @endif
+                                @if (config('access.registration'))
+                                    <a href="{{route('frontend.auth.register')}}" class="nav-link {{ active_class(Active::checkRoute('frontend.auth.register')) }}"><i class="fa fa-lock"></i><span>{{ __('navs.frontend.register') }}</span></a>
+                                @endif
+                            @else
+                                <a href="#" class="nav-link"><i class="fa fa-user-circle"></i><span>{{ $logged_in_user->name }}</span></a>
+                                @can('view backend')
+                                    <a href="{{ route('admin.dashboard') }}" class="nav-link">{{ __('navs.frontend.user.administration') }}</a>
+                                @endcan
 
-                                    @guest
-                                        <a href="{{route('frontend.auth.login')}}" class="nav-link {{ active_class(Active::checkRoute('frontend.auth.login')) }}"><i class="fa fa-sign-in"></i><span>{{ __('navs.frontend.login') }}</span></a>
+                                <a href="{{ route('frontend.user.account') }}" class="nav-link {{ active_class(Active::checkRoute('frontend.user.account')) }}"><i class="fa fa-arrow-circle-o-right"></i><span>{{ __('navs.frontend.user.account') }}</span></a>
+                                <a href="{{ route('frontend.auth.logout') }}" class="nav-link"><i class="fa fa-sign-out"></i><span>{{ __('navs.general.logout') }}</span></a>
 
-                                        @if (config('access.registration'))
-                                            <a href="{{route('frontend.auth.register')}}" class="nav-link {{ active_class(Active::checkRoute('frontend.auth.register')) }}"><i class="fa fa-lock"></i><span>{{ __('navs.frontend.register') }}</span></a>
-                                        @endif
-                                    @else
-                                        <a href="#" class="nav-link"><i class="fa fa-user-circle"></i><span>{{ $logged_in_user->name }}</span></a>
-                                        @can('view backend')
-                                            <a href="{{ route('admin.dashboard') }}" class="nav-link">{{ __('navs.frontend.user.administration') }}</a>
-                                        @endcan
+                            @endguest
 
-                                        <a href="{{ route('frontend.user.account') }}" class="nav-link {{ active_class(Active::checkRoute('frontend.user.account')) }}"><i class="fa fa-arrow-circle-o-right"></i><span>{{ __('navs.frontend.user.account') }}</span></a>
-                                        <a href="{{ route('frontend.auth.logout') }}" class="nav-link"><i class="fa fa-sign-out"></i><span>{{ __('navs.general.logout') }}</span></a>
-
-                                    @endguest
-
-                                    @auth
-                                        <a href="{{route('frontend.user.dashboard')}}" class="nav-link {{ active_class(Active::checkRoute('frontend.user.dashboard')) }}"><i class="fa fa-tachometer"></i><span>{{ __('navs.frontend.dashboard') }}</span></a>
-                                    @endauth
-
-                                </div>
-                            </nav>
-                            <button class="close-button" id="close-button">Close Menu</button>
-
+                            @auth
+                                <a href="{{route('frontend.user.dashboard')}}" class="nav-link {{ active_class(Active::checkRoute('frontend.user.dashboard')) }}"><i class="fa fa-tachometer"></i><span>{{ __('navs.frontend.dashboard') }}</span></a>
+                            @endauth
                         </div>
-
-                        <button class="menu-button" id="open-button">Open Menu</button>
-        </div>
-        
-        <div class="main-menu">
-            <div class="agileinfomenu">
-                <p>MENU</p>
+                    </nav>
+                    <button class="close-button" id="close-button">Close Menu</button>
+                </div>
+                <button class="menu-button" id="open-button">Open Menu</button>
             </div>
-        </div>
 
-        <div class="aitslogow3ls">
-            <a href="/"><div class="agilelogo">My Career</div></a>
-        </div>
+            </div>
 
-        <div class="wthreecontact">
-            <p><a class="scroll" href="#contact"><i class="fa fa-envelope-o" aria-hidden="true"></i> CONTACT ME</a></p>
         </div>
     </div>
 <!-- //Header -->
